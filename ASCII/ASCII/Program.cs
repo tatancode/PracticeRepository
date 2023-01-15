6af13c2772506fe44c3d02f5cf4862da17db454c
Sebastian Bustos <86377478+tatancode@users.noreply.github.com>
2023-01-15 14:33:46 -0600

    ASCII Art

---
+++ ASCII/ASCII/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Building a ASCII Creature
+            Console.WriteLine("  ___");
+            Console.WriteLine(" (o o)");
+            Console.WriteLine("<| O |>");
+            Console.WriteLine(" |   |");
+            Console.WriteLine(" '~~~'");
+            Console.WriteLine("Boo!");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("c_    ___");
+            Console.WriteLine("  \\\\__|__|__");
+            Console.WriteLine("    \\( o_o)");
+            Console.WriteLine("     > ~  >");
+            Console.WriteLine("    /    /\\\\");
+            Console.WriteLine("   /    /  \\\\");
+            Console.WriteLine("   \\    )   c/");
+            Console.WriteLine("    /   /");
+            Console.WriteLine("   /   /|");
+            Console.WriteLine("  (   ( \\");
+            Console.WriteLine("   |  |  \\");
+            Console.WriteLine("   | / \\  )");
+            Console.WriteLine("   | |  ) | ");
+            Console.WriteLine("  /  ) (_/");
+            Console.WriteLine(" (_ /");
+            Console.WriteLine("Dance!");
+        }
+    }
+}
